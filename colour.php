f64188d662d8d8b1ae45088b297ad739fd5578e3
Shyam <92642242+shyambera@users.noreply.github.com>
2023-05-29 22:06:37 +0530

    Update colour.php file
    Update colour.php file

--- colour.php
+++ colour.php
@@ -5,6 +5,7 @@
             echo "My car is ". $ColoR ."</br>";  
             echo "My dog is ". $color ."</br>";  
             echo "My Phone is ". $COLOR ."</br>";  
+            echo "My watch is ". $COLOR ."</br>";  
         ?>  
     </body>  
 </html>  
