70f2bbe2075a3482e733c4953912751b5d9c3671
Shyam <92642242+shyambera@users.noreply.github.com>
2023-06-01 22:43:34 +0530

    Update colour.php
    Update colour.php

--- colour.php
+++ colour.php
@@ -9,3 +9,16 @@
         ?>  
     </body>  
 </html>  
+
+
+<html>  
+    <body>  
+        <?php  
+            $color = "black";  
+            echo "My car is ". $ColoR ."</br>";  
+            echo "My dog is ". $color ."</br>";  
+            echo "My Phone is ". $COLOR ."</br>";  
+            echo "My watch is ". $COLOR ."</br>";  
+        ?>  
+    </body>  
+</html>  
